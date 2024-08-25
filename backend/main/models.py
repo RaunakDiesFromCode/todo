c0cf28370dfec3a5b0f6bb8d5522cb2230a82ab6
captainTigerYT <raunakmanna43@gmail.com>
2024-08-25 21:59:57 +0530

    updated models.py

--- backend/main/models.py
+++ backend/main/models.py
@@ -10,6 +10,7 @@ class TodoList(models.Model):
     title = models.CharField(max_length=200)
     status = models.BooleanField(default=False)
     created_at = models.DateField(default=datetime.datetime.today().date())
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.title
